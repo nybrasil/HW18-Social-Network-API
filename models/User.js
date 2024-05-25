5989a7cc503575ff98337a67b3ba0690ef3a25a7
Tania Bezerra <nybrasil@gmail.com>
2024-05-25 14:58:24 -0400

    created model Reaction.js

--- models/User.js
+++ models/User.js
@@ -29,15 +29,15 @@ const userSchema = new Schema(
           ],
         },
         {
-          // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-          // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
           toJSON: {
             virtuals: true,
             },
           id: false,
         }
     );
-    // Create a virtual property `fullName` that gets and sets the user's full name
+    //Create a virtual called 'friendCount' that retrieves the length of the user's 'friends' array field on query.
      userSchema
      .virtual('friendCount')
      
